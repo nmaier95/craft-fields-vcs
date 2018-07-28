eebf8de591db78adede01b767b34cc723dab474e
Niklas Maier <maier-niklas@t-online.de>
2018-07-28 11:08:34 +0200

    chore: add initial source

---
+++ src/Craftfieldsvcs.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * craft-fields-vcs plugin for Craft CMS 3.x
+ *
+ * Export fields as json to get them into your vcs of choice.  Import them into your cms instance as well.
+ *
+ * @link      https://github.com/nmaier95
+ * @copyright Copyright (c) 2018 Niklas Maier
+ */
+
+namespace nmaier95craftfieldsvcs\craftfieldsvcs;
+
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Plugins;
+use craft\events\PluginEvent;
+use craft\web\UrlManager;
+use craft\events\RegisterUrlRulesEvent;
+
+use yii\base\Event;
+
+/**
+ * Craft plugins are very much like little applications in and of themselves. We’ve made
+ * it as simple as we can, but the training wheels are off. A little prior knowledge is
+ * going to be required to write a plugin.
+ *
+ * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
+ * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
+ *
+ * https://craftcms.com/docs/plugins/introduction
+ *
+ * @author    Niklas Maier
+ * @package   Craftfieldsvcs
+ * @since     1.0.0
+ *
+ */
+class Craftfieldsvcs extends Plugin
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * Static property that is an instance of this plugin class so that it can be accessed via
+     * Craftfieldsvcs::$plugin
+     *
+     * @var Craftfieldsvcs
+     */
+    public static $plugin;
+
+    // Public Properties
+    // =========================================================================
+
+    /**
+     * To execute your plugin’s migrations, you’ll need to increase its schema version.
+     *
+     * @var string
+     */
+    public $schemaVersion = '1.0.0';
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Set our $plugin static property to this class so that it can be accessed via
+     * Craftfieldsvcs::$plugin
+     *
+     * Called after the plugin class is instantiated; do any one-time initialization
+     * here such as hooks and events.
+     *
+     * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
+     * you do not need to load it in your init() method.
+     *
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
+
+        // Register our site routes
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules['siteActionTrigger1'] = 'craft-fields-vcs/default';
+            }
+        );
+
+        // Register our CP routes
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_CP_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules['cpActionTrigger1'] = 'craft-fields-vcs/default/do-something';
+            }
+        );
+
+        // Do something after we're installed
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+            function (PluginEvent $event) {
+                if ($event->plugin === $this) {
+                    // We were just installed
+                }
+            }
+        );
+
+/**
+ * Logging in Craft involves using one of the following methods:
+ *
+ * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
+ * Craft::info(): record a message that conveys some useful information.
+ * Craft::warning(): record a warning message that indicates something unexpected has happened.
+ * Craft::error(): record a fatal error that should be investigated as soon as possible.
+ *
+ * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
+ *
+ * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
+ * the category to the method (prefixed with the fully qualified class name) where the constant appears.
+ *
+ * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
+ * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
+ */
+        Craft::info(
+            Craft::t(
+                'craft-fields-vcs',
+                '{name} plugin loaded',
+                ['name' => $this->name]
+            ),
+            __METHOD__
+        );
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+}
---
+++ src/translations/en/craft-fields-vcs.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * craft-fields-vcs plugin for Craft CMS 3.x
+ *
+ * Export fields as json to get them into your vcs of choice.  Import them into your cms instance as well.
+ *
+ * @link      https://github.com/nmaier95
+ * @copyright Copyright (c) 2018 Niklas Maier
+ */
+
+/**
+ * craft-fields-vcs en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('craft-fields-vcs', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Niklas Maier
+ * @package   Craftfieldsvcs
+ * @since     1.0.0
+ */
+return [
+    'craft-fields-vcs plugin loaded' => 'craft-fields-vcs plugin loaded',
+];
